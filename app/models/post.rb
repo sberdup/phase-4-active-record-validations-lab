d74053abcd3efe75ef48e9407037348061aea7d3
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-22 17:42:36 -0600

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length:{minimum: 250}
+    validates :summary, length:{maximum: 250}
+    validates :category, inclusion:{in: ["Fiction","Non-Fiction"]}
+    validate :no_clickbait 
+
+    def no_clickbait
+        if title == 'True Facts'
+            errors.add(:title, 'Not allowed.')
+        end
+    end
+
 end
